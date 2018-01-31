gradient clipping fix

main.py
@@ -20,6 +20,7 @@ from torch.autograd import Variable
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--clip', default=0.25, type=float, help='gradient clipping value')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 args = parser.parse_args()
 
@@ -70,7 +71,7 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 #testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -98,6 +99,7 @@ def train(epoch):
         outputs = net(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
+        torch.nn.utils.clip_grad_norm(net.parameters(), args.clip)
         optimizer.step()
 
         train_loss += loss.data[0]
@@ -146,6 +148,6 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+50):
     train(epoch)
     test(epoch)